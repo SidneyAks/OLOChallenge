64457c83012b63d2b095bab191a9c04c60097a17
SidneyAks <Sidneyaks@gmail.com>
2020-08-21 02:19:51 -0500

    Created tests for PUT/POST functionality

---
+++ OLOChallenge/Tests/post_Posts.cs
@@ -0,0 +1,192 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OLOChallenge.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Newtonsoft.Json;
+using OLOChallenge.Helpers;
+using OLOChallenge.Authentication;
+using OLOChallenge.Entities;
+
+namespace OLOChallenge.Tests
+{
+    /// <summary>
+    /// Tests the post method on the post route
+    /// </summary>
+    /// 
+    /// <remarks>
+    /// The post operation on the posts route is poorly defined. This appears to be a basic way of teaching how to talk
+    /// to a web service with very little in the way of input validation, it makes it hard to write a tests for it, as
+    /// there isn't much documentation on how it _should_ behave. For the purposes of this exercise, I'm going to have
+    /// to assume some of the business rules.
+    /// 
+    /// 1.) The route is plural. I would expect since it's plural it can take one or more post objects (in fact, it appears
+    /// to accept multiple post objects, but the results don't appear to indicate multiple were saved).
+    /// 
+    /// 2.) The route accepts a defined object but in reality doesn't appear to validate it at all, it simply returns a 201
+    /// even if the data it was passed makes no sense. I would expect there to be some validation (At the very list that 
+    /// the title and body are valid, and that the user id exists!). I'm going to write tests as such, but they will fail.
+    /// If I were in a feedback loop with developers I would hope these requirements were either laid out prior to code authorship 
+    /// (although honestly I'd expect that if a user were trying to create a post and the title and body were both null, throwing
+    /// an error wouldn't need to be an explicit requirement)
+    /// 
+    /// 3.) The post object contains a userId field -- I expect the create or update system would either 1.) Throw an error if the 
+    /// user tries to create a post with a different ID than their own or 2.) Ignore the userID field and create the post with the id
+    /// of the authenticated user (this seems less preferable to me, but I would defer to the product owner) . Of course since we 
+    /// don't actually authenticate into this system it's impossible to test either of the scenarios.
+    /// </remarks>
+    [TestClass]
+    public class post_Posts
+    {
+        [TestMethod]
+        public void WithValidPostIs200AndReturnsPostsWithUpdateFields()
+        {
+            var session = Auth.Authenticate(4);
+            var inputdata = new Post()
+            {
+                title = "Hello World",
+                body = Lorem.Text,
+                userId = session.UserID
+            };
+
+            var data = session.JSONPlaceHolder_post_Posts(inputdata);
+            Assert.AreEqual(HttpStatusCode.Created, data.StatusCode, "Unexpected status code when creating post");
+
+            var createdPost = ExtraAssert.Succeeds(() => (Post)JsonConvert.DeserializeObject(data.Content.ReadAsStringAsync().Result, typeof(Post)),
+                "Unable to parse response into Post");
+
+            Assert.AreEqual(inputdata.title, createdPost.title, "Post returned from creation is incorrect");
+            Assert.AreEqual(inputdata.body, createdPost.body, "Post returned from creation is incorrect");
+            Assert.AreEqual(inputdata.userId, createdPost.userId, "Post returned from creation is incorrect");
+
+            var retrievedPostResponse = session.JSONPlaceHolder_get_Posts(createdPost.id);
+            Assert.AreEqual(HttpStatusCode.OK, retrievedPostResponse.StatusCode, "Unexpected response code when retrieving created post");
+
+            var retrievedPost = ExtraAssert.Succeeds(() => (Post)JsonConvert.DeserializeObject(retrievedPostResponse.Content.ReadAsStringAsync().Result, typeof(Post)),
+                "Unable to parse response into Post");
+
+            Assert.AreEqual(inputdata.title, retrievedPost.title, "Post saved after creation is incorrect");
+            Assert.AreEqual(inputdata.body, retrievedPost.body, "Post saved after creation is incorrect");
+            Assert.AreEqual(inputdata.userId, retrievedPost.userId, "Post saved after creation is incorrect");
+        }
+
+        [TestMethod]
+        public void WithMultipleValidPostsIs200AndReturnsPostsWithUpdateFields()
+        {
+            var session = Auth.Authenticate(4);
+            var inputdata = new Post[]
+            {
+                new Post()
+                {
+                    title = "Hello World",
+                    body = Lorem.Text,
+                    userId = session.UserID
+                },
+                new Post()
+                {
+                    title = "Hello World 2",
+                    body = Lorem.Text,
+                    userId = session.UserID
+                }
+            };
+            var data = session.JSONPlaceHolder_post_Posts(inputdata);
+            Assert.AreEqual(HttpStatusCode.Created, data.StatusCode, "Unexpected status code when creating post");
+
+            var createdPosts = ExtraAssert.Succeeds(() => (List<Post>)JsonConvert.DeserializeObject(data.Content.ReadAsStringAsync().Result, typeof(List<Post>)),
+                "Unable to parse response into list of Posts");
+
+            //Beyond here it's a little hard to debug to make sure the tests run the way I expect them to, as the API does not respond in a way I would
+            //expect it to
+
+            createdPosts.ForEachWithIndex((createdPost, index) =>
+            {
+                Assert.AreEqual(inputdata[index].title, createdPost.title, "Post returned from creation is incorrect");
+                Assert.AreEqual(inputdata[index].body, createdPost.body, "Post returned from creation is incorrect");
+                Assert.AreEqual(inputdata[index].userId, createdPost.userId, "Post returned from creation is incorrect");
+
+                var retrievedPostResponse = session.JSONPlaceHolder_get_Posts(createdPost.id);
+                Assert.AreEqual(HttpStatusCode.OK, retrievedPostResponse.StatusCode, "Unexpected response code when retrieving created posts");
+
+                var retrievedPost = ExtraAssert.Succeeds(() => (Post)JsonConvert.DeserializeObject(retrievedPostResponse.Content.ReadAsStringAsync().Result, typeof(Post)),
+                    "Unable to parse response list of Posts");
+
+                Assert.AreEqual(inputdata[index].title, retrievedPost.title, "Post saved after creation is incorrect");
+                Assert.AreEqual(inputdata[index].body, retrievedPost.body, "Post saved after creation is incorrect");
+                Assert.AreEqual(inputdata[index].userId, retrievedPost.userId, "Post saved after creation is incorrect");
+            });
+        }
+
+        [TestMethod]
+        public void WithoutSessionIs401Error()
+        {
+            var session = SessionObject.UnauthenticatedSession;
+            var data = session.JSONPlaceHolder_post_Posts(
+            new Post()
+            {
+                title = "Hello World",
+                body = Lorem.Text,
+                userId = "1"
+            });
+            Assert.AreEqual(HttpStatusCode.Unauthorized, data.StatusCode);
+        }
+
+        [TestMethod]
+        public void WithInvalidSessionIs401Error()
+        {
+            var session = SessionObject.InvalidSession;
+            var data = session.JSONPlaceHolder_post_Posts(
+            new Post()
+            {
+                title = "Hello World",
+                body = Lorem.Text,
+                userId = "1"
+            });
+            Assert.AreEqual(HttpStatusCode.Unauthorized, data.StatusCode);
+        }
+
+        [TestMethod]
+        public void WithNullTitleIs400Error()
+        {
+            var session = Auth.Authenticate(4);
+            var data = session.JSONPlaceHolder_post_Posts(
+                new Post()
+                {
+                    title = null,
+                    body = Lorem.Text,
+                    userId = session.UserID
+                });
+            Assert.AreEqual(HttpStatusCode.BadRequest, data.StatusCode);
+        }
+
+        [TestMethod]
+        public void WithNullBodyIs400Error()
+        {
+            var session = Auth.Authenticate(4);
+            var data = session.JSONPlaceHolder_post_Posts(
+                new Post()
+                {
+                    title = "Hello World",
+                    body = null,
+                    userId = session.UserID
+                });
+            Assert.AreEqual(HttpStatusCode.BadRequest, data.StatusCode);
+        }
+
+        [TestMethod]
+        public void WithIncorrectUserIDIs401Error()
+        {
+            var session = Auth.Authenticate(4);
+            var data = session.JSONPlaceHolder_post_Posts(
+                new Post()
+                {
+                    title = "Hello World",
+                    body = Lorem.Text,
+                    userId = "1"
+                });
+            Assert.AreEqual(HttpStatusCode.Unauthorized, data.StatusCode);
+        }
+    }
+}
---
+++ OLOChallenge/Tests/put_Posts.cs
@@ -0,0 +1,169 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using OLOChallenge.Authentication;
+using OLOChallenge.Entities;
+using OLOChallenge.Helpers;
+using OLOChallenge.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OLOChallenge.Tests
+{
+    /// <summary>
+    /// Tests the post method on the post route.
+    /// </summary>
+    /// 
+    /// <remarks>
+    /// This API is a hair more straightforeward than the those of the post verb. Since the url takes a single parameter (the post id) and 
+    /// must have it, it appears only one post can be modified at a time (which makes sense). Given that restraint it should be a bit easier
+    /// to test, however we do have some basic things we still need to check for, such as if the session ID is checked and if the updated
+    /// post object contains valid data.
+    /// </remarks>
+    [TestClass]
+    public class put_Posts
+    {
+        private static void CreateGenericPostForPrecondition(SessionObject session, out Post CreatedPost)
+        {
+            CreatedPost = null;
+
+            var inputdata = new Post()
+            {
+                title = "Hello World",
+                body = Lorem.Text,
+                userId = session.UserID
+            };
+
+            var data = session.JSONPlaceHolder_post_Posts(inputdata);
+
+            CreatedPost = ExtraAssert.Succeeds(() => (Post)JsonConvert.DeserializeObject(data.Content.ReadAsStringAsync().Result, typeof(Post)),
+                "Unable to parse response into Post");
+
+            Assert.IsNotNull(CreatedPost, "Generic Post for test Is Null");
+        }
+
+        [TestMethod]
+        public void WithExistingPostAndValidDataIs200AndReturnsPostWithUpdatedFields()
+        {
+            var session = Auth.Authenticate(4);
+            Post CreatedPost = null;
+
+            ExtraAssert.PreconditionSucceeds(() => CreateGenericPostForPrecondition(session, out CreatedPost), "Unable to create post to modify with put verb");
+
+            CreatedPost.title += "Modified";
+            CreatedPost.body += "Modified";
+
+            var data = session.JSONPlaceHolder_put_Posts(CreatedPost.id,CreatedPost);
+            Assert.AreEqual(HttpStatusCode.OK, data.StatusCode, "Unexpected status code when modifying post");
+
+            var updatedPost = ExtraAssert.Succeeds(() => (Post)JsonConvert.DeserializeObject(data.Content.ReadAsStringAsync().Result, typeof(Post)),
+                "Unable to parse response into Post");
+
+            Assert.AreEqual(CreatedPost.title, updatedPost.title, "Post returned from modification is incorrect");
+            Assert.AreEqual(CreatedPost.body, updatedPost.body, "Post returned from modification is incorrect");
+            Assert.AreEqual(CreatedPost.userId, updatedPost.userId, "Post returned from modification is incorrect");
+
+            var retrievedPostResponse = session.JSONPlaceHolder_get_Posts(updatedPost.id);
+            Assert.AreEqual(HttpStatusCode.OK, retrievedPostResponse.StatusCode, "Unexpected response code when retrieving modification post");
+
+            var retrievedPost = ExtraAssert.Succeeds(() => (Post)JsonConvert.DeserializeObject(retrievedPostResponse.Content.ReadAsStringAsync().Result, typeof(Post)),
+                "Unable to parse response into Post");
+
+            Assert.AreEqual(CreatedPost.title, retrievedPost.title, "Post saved after modification is incorrect");
+            Assert.AreEqual(CreatedPost.body, retrievedPost.body, "Post saved after modification is incorrect");
+            Assert.AreEqual(CreatedPost.userId, retrievedPost.userId, "Post saved after modification is incorrect");
+        }
+
+        [TestMethod]
+        public void WithoutSessionis401Error()
+        {
+            Post CreatedPost = null;
+            ExtraAssert.PreconditionSucceeds(() => CreateGenericPostForPrecondition(Auth.Authenticate(4), out CreatedPost), "Unable to create post to modify with put verb");
+
+            var data = SessionObject.UnauthenticatedSession.JSONPlaceHolder_put_Posts(CreatedPost.id, CreatedPost);
+            Assert.AreEqual(HttpStatusCode.Unauthorized, data.StatusCode);
+        }
+
+        [TestMethod]
+        public void WithInvalidSessionIs401Error()
+        {
+            Post CreatedPost = null;
+            ExtraAssert.PreconditionSucceeds(() => CreateGenericPostForPrecondition(Auth.Authenticate(4), out CreatedPost), "Unable to create post to modify with put verb");
+
+            var data = SessionObject.InvalidSession.JSONPlaceHolder_put_Posts(CreatedPost.id, CreatedPost);
+            Assert.AreEqual(HttpStatusCode.Unauthorized, data.StatusCode);
+        }
+
+        [TestMethod]
+        public void WithNullTitleIs400Error()
+        {
+            var session = Auth.Authenticate(4);
+
+            Post CreatedPost = null;
+            ExtraAssert.PreconditionSucceeds(() => CreateGenericPostForPrecondition(session, out CreatedPost), "Unable to create post to modify with put verb");
+
+            CreatedPost.title = null;
+            var data = SessionObject.InvalidSession.JSONPlaceHolder_put_Posts(CreatedPost.id, CreatedPost);
+
+            Assert.AreEqual(HttpStatusCode.BadRequest, data.StatusCode);
+        }
+
+        [TestMethod]
+        public void WithNullBodyIs400Error()
+        {
+            var session = Auth.Authenticate(4);
+
+            Post CreatedPost = null;
+            ExtraAssert.PreconditionSucceeds(() => CreateGenericPostForPrecondition(session, out CreatedPost), "Unable to create post to modify with put verb");
+
+            CreatedPost.body = null;
+            var data = SessionObject.InvalidSession.JSONPlaceHolder_put_Posts(CreatedPost.id, CreatedPost);
+
+            Assert.AreEqual(HttpStatusCode.BadRequest, data.StatusCode);
+        }
+
+        [TestMethod]
+        public void WithIncorrectUserIDIs401Error()
+        {
+            var session = Auth.Authenticate(4);
+
+            Post CreatedPost = null;
+            ExtraAssert.PreconditionSucceeds(() => CreateGenericPostForPrecondition(session, out CreatedPost), "Unable to create post to modify with put verb");
+
+            CreatedPost.userId = "1";
+            var data = SessionObject.InvalidSession.JSONPlaceHolder_put_Posts(CreatedPost.id, CreatedPost);
+            Assert.AreEqual(HttpStatusCode.Unauthorized, data.StatusCode);
+        }
+
+        [TestMethod]
+        public void WithNonExistentPostIdIs404Error()
+        {
+            var session = Auth.Authenticate(4);
+
+            Post CreatedPost = null;
+            ExtraAssert.PreconditionSucceeds(() => CreateGenericPostForPrecondition(session, out CreatedPost), "Unable to create post to modify with put verb");
+
+            //Well assume that there are not this many posts in the database.
+            CreatedPost.id = Int64.MaxValue.ToString();
+            var data = SessionObject.InvalidSession.JSONPlaceHolder_put_Posts(CreatedPost.id, CreatedPost);
+            Assert.AreEqual(HttpStatusCode.NotFound, data.StatusCode);
+        }
+
+        [TestMethod]
+        public void WithInconsistentIDBetweenUrlandBodyIs400Error()
+        {
+            var session = Auth.Authenticate(4);
+
+            Post CreatedPost = null;
+            ExtraAssert.PreconditionSucceeds(() => CreateGenericPostForPrecondition(session, out CreatedPost), "Unable to create post to modify with put verb");
+
+            //Well assume that there are not this many posts in the database.
+            var data = SessionObject.InvalidSession.JSONPlaceHolder_put_Posts("1", CreatedPost);
+            Assert.AreEqual(HttpStatusCode.BadRequest, data.StatusCode);
+        }
+
+    }
+}
